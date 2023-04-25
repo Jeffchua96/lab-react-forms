eab232afcbb20a8c24effad88e40e3ed970bdf5c
Jeffchua96 <jeffersonchua@pursuit.org>
2023-04-25 17:56:25 -0400

    not yet

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,24 @@
 import React from "react";
-import "./Form.css";
+import Form from "./Form.css";
+import { useState } from "react";
+import values from "react";
+
 
 function Form() {
+const [values, setValues] = useState("")
+
+  function handleValueInputChange (event) {
+    setValues(event.target.value)
+  }
+
+  function setValueChange(event){
+    setValues(event.setOption)
+  }
+
   return (
     <>
       <form>
-        <input id="values" name="values" type="text" />
+        <input id="values" name="values" type="text" onChange={handleValueInputChange} />
         <select id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
